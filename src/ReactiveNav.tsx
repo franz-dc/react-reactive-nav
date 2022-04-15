9e9fc735e9d77bb8604f9086bbe4747e1b3f4253
Franz Joshua Dela Cruz <franzdc.21@gmail.com>
2022-04-16 03:52:27 +0800

    Add snap feature

--- src/ReactiveNav.tsx
+++ src/ReactiveNav.tsx
@@ -16,6 +16,20 @@ export interface ReactiveNavProps extends HTMLAttributes<HTMLDivElement> {
    */
   height: number;
   /**
+   * Snap to the closest state of the navbar based on the scroll amount.
+   */
+  snap?: boolean;
+  /**
+   * Set the delay of snap checking after scrolling (in ms).
+   * Only applies if `snap` is set to `true`.
+   */
+  snapDelay?: number;
+  /**
+   * Specify how long the snap animation should run (in ms).
+   * Only applies if `snap` is set to `true`.
+   */
+  snapDuration?: number;
+  /**
    * Props passed to the padding element (blank div with a set height).
    */
   paddingElementProps?: HTMLAttributes<HTMLDivElement>;
@@ -24,21 +38,36 @@ export interface ReactiveNavProps extends HTMLAttributes<HTMLDivElement> {
 export const ReactiveNav: FC<ReactiveNavProps> = ({
   children,
   height,
+  snap,
+  snapDelay = 200,
+  snapDuration = 200,
   style,
   paddingElementProps,
   ...rest
 }) => {
-  const [yScrollAmount, setYScrollAmount] = useState(0);
-
   const { y } = useWindowScrollPosition({ throttle: 0 });
 
   const [lastYPos, setLastYPos] = useState(y);
+  const [yScrollAmount, setYScrollAmount] = useState(0);
+  const [isSnapping, setIsSnapping] = useState(false);
+  const [isTouching, setIsTouching] = useState(false);
 
   useEffect(() => {
+    // check touch state to prevent snap when scrolling on touch devices
+    // TODO: do the same for mouse devices
+    const handleTouchStart = () => setIsTouching(true);
+    const handleTouchEnd = () => setIsTouching(false);
+
+    window.addEventListener('touchstart', handleTouchStart);
+    window.addEventListener('touchend', handleTouchEnd);
+
+    // change navbar position based on scroll amount
     const updateScrollPos = () => {
       const yPosDiff = y - lastYPos;
 
       if (yPosDiff === 0) return;
+
+      setIsSnapping(false);
 
       setYScrollAmount((sa) => {
         const newYScrollAmount = sa + yPosDiff;
@@ -48,18 +77,55 @@ export const ReactiveNav: FC<ReactiveNavProps> = ({
       });
 
       setLastYPos(y);
+
+      setTimeout(() => {
+        setIsSnapping(true);
+      }, snapDelay);
     };
 
     window.addEventListener('scroll', updateScrollPos);
+
     updateScrollPos();
-    return () => window.removeEventListener('scroll', updateScrollPos);
-  }, [y, yScrollAmount, lastYPos, height]);
+
+    return () => {
+      window.removeEventListener('scroll', updateScrollPos);
+      window.removeEventListener('touchstart', handleTouchStart);
+      window.removeEventListener('touchend', handleTouchEnd);
+    };
+  }, [
+    // states
+    y,
+    yScrollAmount,
+    lastYPos,
+    isSnapping,
+    isTouching,
+    // props
+    height,
+    snap,
+    snapDelay,
+    snapDuration,
+  ]);
+
+  useEffect(() => {
+    if (
+      snap &&
+      !isTouching &&
+      isSnapping &&
+      y >= height &&
+      yScrollAmount !== 0 &&
+      yScrollAmount !== height
+    ) {
+      const halfHeight = height / 2;
+      setYScrollAmount((sa) => (sa < halfHeight ? 0 : height));
+    }
+  }, [y, yScrollAmount, height, snap, isSnapping, isTouching]);
 
   return (
     <>
       <div
         {...rest}
         style={{
+          transition: isSnapping ? `transform ${snapDuration}ms` : '',
           position: 'fixed',
           width: '100%',
           height,
